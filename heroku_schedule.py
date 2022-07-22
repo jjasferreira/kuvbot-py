4692d8353007105b77d4009bab1ae5ec65acb6f1
José João Ferreira <jjasferreira@gmail.com>
2022-07-22 18:44:05 +0100

    Further customize heroku schedule

--- heroku_schedule.py
+++ heroku_schedule.py
@@ -1,7 +1,12 @@
+# Python modules
+from time import sleep
+
+# Import actions to conduct
 from actions import *
 
-# Runs every 10 minutes
+
 def main():
+    """Runs every 10 minutes."""
 
     # If there is a file with a counter, load it; otherwise, initialize the counter
     if not path.exists(JSON_DIR + "/heroku_schedule.json"):
@@ -14,13 +19,15 @@ def main():
     # Account for the new 10 minutes that have passed
     times += 1
 
-    # 01. Every 10 minutes, likes a tweet containing certain word(s)
-    if times % 1 == 0:
-        action_like_tweet_contains("#photography")
-    
-    # 02. Every 3 hours, tweets an image
+    # 01. Every 3 hours, tweets an image
     if times % 18 == 0:
         action_tweet_image_picsum(1000)
+
+    # 02. Every minute, likes a tweet containing certain word(s)
+    if times % 1 == 0:
+        for i in range(0, 10):
+            action_like_tweet_contains("#photography")
+            sleep(60)
 
     # (Over)write counter variable to file
     file = open(JSON_DIR + "/heroku_schedule.json", "w")
